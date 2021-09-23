77aa06acc016fea8f316a22aa4acc30781e9f793
itutusausb <itutusausb@uoc.edu>
2021-09-23 23:43:02 +0200

    comentario isaac tutusaus en MainActivity

--- app/src/main/java/com/example/test/MainActivity.java
+++ app/src/main/java/com/example/test/MainActivity.java
@@ -32,6 +32,11 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
+
+        //funcionamiento github
+        /*
+         * comentario Isaac tutusaus
+         */
     }
 
 }
